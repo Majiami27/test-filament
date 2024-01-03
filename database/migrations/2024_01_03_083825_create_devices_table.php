f65343245355c8f9ee93c47abf345ef93f06d65f
Majiami27 <mingko.chen@justech.com.tw>
2024-01-03 17:32:49 +0800

    假資料生成

---
+++ database/migrations/2024_01_03_083825_create_devices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('mac_address', 30)->index();
+            $table->string('name', 30);
+            $table->string('custom_id', 30);
+            $table->foreignId('area_id', 30)->nullable();
+            $table->string('ip', 30);
+            $table->string('ssid', 32);
+            $table->unsignedTinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
