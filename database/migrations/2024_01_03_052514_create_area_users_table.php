3c3a38c2e97c47ea1ce3cf60b93b674affb3392d
luca.lin <luca.lin@justech.com.tw>
2024-01-03 13:30:14 +0800

    feat: 🎸 add area user seeder

--- database/migrations/2024_01_03_052514_create_area_users_table.php
+++ database/migrations/2024_01_03_052514_create_area_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('area_users', function (Blueprint $table) {
+        Schema::create('area_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('area_id')->constrained('areas')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
