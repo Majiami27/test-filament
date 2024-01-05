158ec497da3dcc25378a2b76ee11c816b37af370
luca.lin <luca.lin@justech.com.tw>
2024-01-05 16:35:48 +0800

    fix: 🐛 admin user create resource, add organization_id

--- app/Filament/Resources/AreaResource/Pages/CreateArea.php
+++ app/Filament/Resources/AreaResource/Pages/CreateArea.php
@@ -3,10 +3,16 @@
 namespace App\Filament\Resources\AreaResource\Pages;
 
 use App\Filament\Resources\AreaResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateArea extends CreateRecord
 {
     protected static string $resource = AreaResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['organization_id'] = auth()->id();
+
+        return $data;
+    }
 }
