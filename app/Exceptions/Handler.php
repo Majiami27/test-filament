2d0024ea44dabbc6862ed745c3f2a33cf9ff67cf
luca.lin <luca.lin@justech.com.tw>
2024-01-19 16:09:25 +0800

    fix: 🐛 403 404 redirector to dashboard, remove weclome

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\App;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -17,6 +20,35 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof AuthenticationException) {
+            return (new Redirector(App::get('url')))->route('login');
+        }
+
+        if ($e instanceof \Spatie\Permission\Exceptions\UnauthorizedException) {
+            return (new Redirector(App::get('url')))->route('filament.admin.pages.dashboard');
+        }
+
+        if ($e instanceof \Symfony\Component\HttpKernel\Exception\HttpException) {
+            if ($e->getStatusCode() === 403) {
+                return (new Redirector(App::get('url')))->route('filament.admin.pages.dashboard');
+            }
+        }
+
+        if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+            return (new Redirector(App::get('url')))->route('filament.admin.pages.dashboard');
+        }
+
+        return parent::render($request, $e);
+    }
 
     /**
      * Register the exception handling callbacks for the application.
